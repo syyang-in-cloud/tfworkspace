4eefb9e981e9f2d812417aa71edc7dbf0ee91f49
SeungYeop Yang <seungyeop.yang@ibm.com>
2021-10-26 23:55:51 -0500

    debug workspace

--- tests/tutorials/vpc-3tier-networking/prod_test.sh
+++ tests/tutorials/vpc-3tier-networking/prod_test.sh
@@ -5,17 +5,32 @@ IBM_PROD_REGION=('us-south')
 
 for i in {1..100}
 do
+
 	echo "Trial: $i"
+
 	for region in "${IBM_PROD_REGION[@]}"
 	do
-		date
 		echo $region
 		echo "region = \"${region}\"" > ${region}.tfvars
 		terraform workspace new $region -no-color
+	done
+
+	for region in "${IBM_PROD_REGION[@]}"
+	do
+		echo $region
+		terraform workspace select $region -no-color
 		terraform init -no-color
 		terraform apply -no-color -auto-approve -var-file="${region}.tfvars"
-		TF_LOG=debug terraform destroy -no-color -auto-approve -var-file="${region}.tfvars"
-		#terraform workspace select default -no-color
-		#terraform workspace delete $region -no-color
+		terraform destroy -no-color -auto-approve -var-file="${region}.tfvars"
 	done
+
+	terraform workspace select default -no-color
+
+	for region in "${IBM_PROD_REGION[@]}"
+	do
+		echo $region
+		terraform workspace delete $region -no-color
+		rm ${region}.tfvars
+	done
+
 done
--- tests/tutorials/vpc-3tier-networking/prod_test_destroy.sh
+++ tests/tutorials/vpc-3tier-networking/prod_test_destroy.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 # ./prod_test_destroy.sh | tee prod_test_destroy.log
 IBM_PROD_REGION=('us-south' 'us-east' 'au-syd' 'br-sao' 'ca-tor' 'eu-de' 'eu-gb' 'jp-osa')
-#IBM_PROD_REGION=('us-south')
 
 for region in "${IBM_PROD_REGION[@]}"
 do
 	date
 	echo $region
 	echo "region = \"${region}\"" > ${region}.tfvars
-	terraform workspace new $region -no-color
-	#terraform init -no-color
-	#terraform apply -no-color -auto-approve -var-file="${region}.tfvars"
+	terraform workspace select $region -no-color
 	TF_LOG=debug terraform destroy -no-color -auto-approve -var-file="${region}.tfvars"
-	#terraform workspace select default -no-color
-	#terraform workspace delete $region -no-color
+	terraform workspace select default
+	terraform workspace delete $region -no-color
+	rm ${region}.tfvars
 done
